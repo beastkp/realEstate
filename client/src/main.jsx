f57c88dfaa7a396ea8d470fe45e1394c8201b213
beastkp <kroshpan@gmail.com>
2023-10-28 10:42:44 +0530

    Added redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./redux/store.js";
+import { store, persistor} from "./redux/store.js";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux"; // provider provides the redux store to the entire react component tree, thi is important because redux is a global state management solution
+import { PersistGate } from "redux-persist/integration/react";
 
 
 const container = document.getElementById("root");
@@ -13,7 +14,9 @@ const root = createRoot(container);
 root.render(
   <>
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>
   </>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,27 @@
-import  {configureStore} from '@reduxjs/toolkit';
+import  {combineReducers, configureStore,getDefaultMiddleware} from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({user:userReducer}) // we need to combine the redicers first in order to persist them- store the values of the reducers in the local storage 
+
+// as we are using the currentUser which stores the current user upon reloading the state of the reducer become reset to initalstate, so to store the values in local storage we use redux-persist library
+
+
+const persistConfig = {
+    key:'root',
+    storage,
+    version:1,
+}
+const persistedReducer = persistReducer(persistConfig,rootReducer)
 
 export const store =  configureStore({
-    reducer:{
-        user: userReducer
-    },
-    // middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    //     serializableCheck:false,
-    // }), // to prvent errors in bowser
+    reducer:persistedReducer,
+    // reducer:{ user: userReducer},
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck:false,
+    }), // to prevent errors in bowser
 })
+
+export const persistor =  persistStore(store) // this will make the store persist 
