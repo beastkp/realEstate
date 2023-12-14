34b98a195a12acda91cf41aaabdbfef58c4f424c
beastkp <kroshpan@gmail.com>
2023-12-14 23:58:46 +0530

    Add Pre-Installation

--- Backend/index.js
+++ Backend/index.js
@@ -6,20 +6,23 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from './routes/listing.route.js'
 import cookieParser from "cookie-parser";
+import path from 'path';
 dotenv.config();
 
 import {error} from "./middleware/error-handler.js";
 
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("Connected to MongoDB!");
+})
+.catch((err) => {
+  console.log(err);
+});
+
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -40,5 +43,12 @@ app.listen(3000, () => {
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+//  if you get any endpoint apart from the above mentioned endpoints
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html')); 
+})
 
 app.use(error);
